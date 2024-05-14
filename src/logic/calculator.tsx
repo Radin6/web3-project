9cbd3bd9d2448f340f2b1ed42253b2f5d4fc186e
Radin6 <30755101+Radin6@users.noreply.github.com>
2024-05-14 18:38:11 +0200

    working

--- src/logic/calculator.tsx
+++ src/logic/calculator.tsx
@@ -14,21 +14,34 @@ export function calculateResult({
   valueB,
 }: calculateResultProps) {
 
+  const result = {message: '', quantity: 0}
+
   // Total number of Token A if all were converted to Token A
   const totalInA = ATokensOwn + BTokensOwn/valueB;
 
   // Total number of Token B if all were converted to Token B
   const totalInB = BTokensOwn*valueB + BTokensOwn;
 
+  // Ratio of A over B
   const ratioAB = ATokensProp/BTokensProp;
 
+  // Percentage of A over B if we convert A to B equvalent
+  const percentageAB = ATokensProp*valueB/(ATokensProp*valueB+BTokensProp)
+
   if (ATokensOwn/ratioAB < BTokensOwn) {
-    console.log("Necesitas convertir Tokens A en Tokens B");
+    result.message = "Necesitas convertir Tokens B en Tokens A";
+    const newQuantity = totalInB - totalInB*(1-percentageAB) - ATokensOwn*valueB;
+    result.quantity = newQuantity;
+
   } else if (ATokensOwn/ratioAB == BTokensOwn) {
-    console.log("Están en proporción");
+    result.message = "Están en proporción";
+    result.quantity = 0;
+
   } else {
-    console.log("Necesitas convertir Tokens B en Tokens A");
+    result.message = "Necesitas convertir Tokens A en Tokens B";
+    const newQuantity = totalInA - totalInA*percentageAB - BTokensOwn/valueB;
+    result.quantity = newQuantity;
   }
   
-  return ("")
+  return (result)
 }
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,14 +1,35 @@
-import {useState} from 'react';
+import { useState } from 'react';
+import { calculateResult } from '../../logic/calculator';
 
-const initialResult = {tokenA: null, tokenB: null}
+const initialResult = {
+  message: '',
+  quantity: 0,
+}
+
+const initialInput = {
+  ATokensOwn: 0,
+  BTokensOwn: 0,
+  ATokensProp: 0,
+  BTokensProp: 0,
+  valueB: 0,
+}
 
 export default function Home() {
   const [result, setResult] = useState(initialResult);
+  const [inputData, setInputData] = useState(initialInput);
 
   const handleReset = () => {
     const inputElem = document.querySelectorAll('input');
     [...inputElem].map((elem) => elem.value = '');
-    setResult(initialResult)
+    setResult(initialResult);
+    setInputData(initialInput);
+  }
+
+  const handleCalculate = () => {
+    console.log(inputData);
+    const newResult  = calculateResult(inputData);
+    setResult(newResult);
+    console.log("Result: ", newResult);
   }
   
   return (
@@ -19,14 +40,14 @@ export default function Home() {
           <section>
             <h2 className="text-lg">Proporción</h2>
             <p>Ingresar la proporción de tokens que te pide la plataforma
-              (por ejemplo si ponés 1 TokenA en la plataforma cuanto de pide de TokenB)</p>
+              (por ejemplo si ponés 1 Token A en la plataforma cuanto de pide de Token B)</p>
             <div className="m-1">
               <label htmlFor="" className="p-1 mr-2">Token A</label>
-              <input type="number" className="p-1 rounded-md bg-gray-light" placeholder="1" />
+              <input onChange={(e)=>setInputData({...inputData, ATokensProp: Number(e.target.value)})} type="number" className="p-1 rounded-md bg-gray-light" placeholder="1" />
             </div>
             <div className="m-1">
               <label htmlFor="" className="p-1 mr-2">Token B</label>
-              <input type="number" className="p-1 rounded-md bg-gray-light" placeholder="1.6" />
+              <input onChange={(e)=>setInputData({...inputData, BTokensProp: Number(e.target.value)})} type="number" className="p-1 rounded-md bg-gray-light" placeholder="1.6" />
             </div>
           </section>
           <section>
@@ -34,11 +55,11 @@ export default function Home() {
             <p>Ingresar la cantidad de cada token que posees</p>
             <div className="m-1">
               <label htmlFor="" className="p-1 mr-2">Token A</label>
-              <input type="number" className="p-1 rounded-md bg-gray-light" placeholder="8.11" />
+              <input onChange={(e)=>setInputData({...inputData, ATokensOwn: Number(e.target.value)})} type="number" className="p-1 rounded-md bg-gray-light" placeholder="8.11" />
             </div>
             <div className="m-1">
               <label htmlFor="" className="p-1 mr-2">Token B</label>
-              <input type="number" className="p-1 rounded-md bg-gray-light" placeholder="2.87" />
+              <input onChange={(e)=>setInputData({...inputData, BTokensOwn: Number(e.target.value)})} type="number" className="p-1 rounded-md bg-gray-light" placeholder="2.87" />
             </div>
           </section>
           <section>
@@ -46,12 +67,20 @@ export default function Home() {
             <p>Ingresar cuanto te darían del Token B si intercambiás 1 Token A por Tokens B (swap)</p>
             <div className="m-1">
               <label htmlFor="" className="p-1 mr-2">Token B</label>
-              <input type="number" className="p-1 rounded-md bg-gray-light" placeholder="1.17" />
+              <input onChange={(e)=>setInputData({...inputData, valueB: Number(e.target.value)})} type="number" className="p-1 rounded-md bg-gray-light" placeholder="1.17" />
             </div>
           </section>
-          <section className='flex gap-2'>
-            <button className="bg-purple p-2 rounded-xl">Calcular</button>
-            <button className="bg-orange p-2 rounded-xl" onClick={handleReset}>Reset</button>
+          <section className='flex gap-2 my-2'>
+            <button onClick={handleCalculate} className="bg-purple p-2 rounded-xl">Calcular</button>
+            <button onClick={handleReset} className="bg-orange p-2 rounded-xl">Reset</button>
+          </section>
+          <section className='mx-auto'>
+          { (0 < result.quantity) && 
+              <div className='m-2 bg-yellow p-2 rounded-lg'>
+                <p>{result.message}</p>
+                <p>Cantidad: {result.quantity}</p>
+              </div>
+            }
           </section>
         </div>
       </div>
